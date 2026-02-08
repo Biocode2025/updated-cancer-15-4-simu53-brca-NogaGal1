c02fd8fb7491908837f3dfe855a221ea2903cd5a
NogaGal1 <nogagal311@gmail.com>
2026-02-08 17:11:49 +0200

    commit main

--- scripts/simu53_BRCA.py
+++ scripts/simu53_BRCA.py
@@ -68,3 +68,78 @@ def RNA_prot(rna_seq):
             break
         protein += RNA_codon_table[codon]
     return protein
+
+
+#### main program ####
+
+def simu53_BRCA():
+
+    Read_dict()
+
+    orgnl_seq = ""
+    with open(os.path.join(data_dir, "human_p53_coding.txt"), "r") as f:
+        for line in f:
+            line = line.strip().upper()
+            if not line.startswith(">"):
+                orgnl_seq += line
+
+    orig_prot = RNA_prot(DNA_RNA_Cod(orgnl_seq))
+
+    user_input = input("Does the Female has a BRCA1,2 mutation? (Y=Yes, N=No) ").upper()
+
+    has_BRCA = (user_input == "Y")
+
+    runs = 1000
+    total_generations = 0
+
+    for run in range(runs):
+
+        generations = 0
+        mutated_seq = orgnl_seq
+        mutation_count = 0
+
+        while True:
+            generations += 1
+
+            if random.random() < 0.0001:
+
+                mutations = random.random()
+
+                if mutations < 0.98:
+                    mutated_seq = Mutate_DNA(mutated_seq)
+                    mutated_prot = RNA_prot(DNA_RNA_Cod(mutated_seq))
+
+                    if Comp_seq(orig_prot, mutated_prot) > 0:
+                        mutation_count += 1
+                        orig_prot = mutated_prot
+
+                elif mutations < 0.99:
+                    mutated_seq = Insert_DNA(mutated_seq)
+                    mutation_count += 1
+
+                else:
+                    mutated_seq = Delete_DNA(mutated_seq)
+                    mutation_count += 1
+
+            if has_BRCA and mutation_count >= 1:
+                break
+
+            if not has_BRCA and mutation_count >= 2:
+                break
+
+        total_generations += generations
+
+    avg_generations = total_generations / runs
+
+    avg_years = avg_generations / 365
+
+    with open(os.path.join(results_dir, "results.txt"), "w") as f:
+        if has_BRCA:
+            f.write("For a female that DOES have BRCA1,2 Mutation:\n")
+        else:
+            f.write("For a female that does NOT have BRCA1,2 Mutation:\n")
+
+        f.write("The mutation that will change the P53 protein will take in average " + str(round(avg_years, 2)) + " years.")
+
+
+simu53_BRCA()
